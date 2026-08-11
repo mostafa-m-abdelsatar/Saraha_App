66cc6ad5e8f40dd0e4c7d89f65379ea68025a30e
Mostafa Mohammed Abdelsatar <mosasa1112@gmail.com>
2026-08-11 03:45:42 +0300

    Deploy edition

--- app.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import bootstrap from "./src/app.controller.js"
-const app = express()
-
-bootstrap(app, express)
-
-export default app
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import path from "node:path"
 import dotenv from "dotenv"
-dotenv.config({path:path.resolve("./src/config/.env.prod")})
-import app from "./app.js";
+console.log(process.env.MOOD);
+if (process.env.MOOD != "PROD"){
+    dotenv.config({path:path.resolve("./src/config/.env.prod")})
+}
+import express from "express"
+import bootstrap from "./src/app.controller.js"
 import DBConnection from "./src/DB/DB.connection.js";
-const port = process.env.PORT || 1206
+const app = express()
+const port = process.env.PORT || 1206;
 
 await DBConnection()
+bootstrap(app, express)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+if (process.env.MOOD != "PROD"){
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
+
+export default app
