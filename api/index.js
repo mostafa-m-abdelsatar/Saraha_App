c495912aa1b9660fbcef68f5799ebd78ef6b16d2
Mostafa Mohammed Abdelsatar <mosasa1112@gmail.com>
2026-08-11 03:08:02 +0300

    Deploy edition

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ import app from "../app.js";
 import DBConnection from "../src/DB/DB.connection.js";
 
 export default async function handler(req, res) {
+    console.log(">>> API HANDLER START");
+
     await DBConnection();
 
+    console.log(">>> DB CONNECTION FINISHED");
     return app(req, res);
 }
--- src/DB/DB.connection.js
+++ src/DB/DB.connection.js
@@ -1,18 +1,29 @@
 import mongoose from "mongoose";
 
-let cachedConnection = null
+let cachedConnection = null;
 
 export default async function DBConnection() {
+    console.log(">>> DBConnection CALLED");
+
+    if (cachedConnection && mongoose.connection.readyState === 1) {
+        console.log(">>> USING CACHED CONNECTION");
+        return cachedConnection;
+    }
+
     try {
-        if (cachedConnection) {
-            return cachedConnection;
-        }
+        console.log(">>> CONNECTING TO DB");
+
         cachedConnection = await mongoose.connect(process.env.DB_URI);
-        console.log("DB connection DONE");
-        return cachedConnection
+
+        console.log(">>> DB connection DONE");
+
+        return cachedConnection;
+
     } catch (error) {
         cachedConnection = null;
-        console.error("DB connection failed:", error);
+
+        console.error(">>> DB connection FAILED:", error);
+
         throw error;
     }
 }
